c2126f914e6c197a5ef138ab4810d2a3c6ff1a71
justinvau <66910213+justinvau@users.noreply.github.com>
2020-06-14 22:48:09 +0600

    Update jsvau.py

--- jsvau.py
+++ jsvau.py
@@ -59,7 +59,7 @@ logo = """ -----------------------------•◈•
 \033[1;96mAuthor  \033[1;93m: \033[1;92mJustin Imran
 \033[1;96mInstagram \033[1;93m: \033[1;FlowImran
 \033[1;96mFacebook  \033[1;93m: \033[1; Tonmoy Chowdhury 
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/justin/zero
 \033[1;91m======================================="""
 
 def tik():
