fc657c421af93701ec5a598065c2c6732a942f50
justinvau <66910213+justinvau@users.noreply.github.com>
2020-06-14 20:36:28 +0600

    Update and rename zeroboy.py to jsvau.py

--- jsvau.py
+++ jsvau.py
@@ -49,9 +49,9 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•SYLHET•◈•GANG™
+|__\~~) •||•JUSTIN - IMRAN---------------•◈•
+|__(-----\  •◈•------BLACK-MIND--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mJustin Imran
+\033[1;96mInstagram \033[1;93m: \033[1;92mJustin79
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Tonmoy Chowdhury 
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Justin/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "imran"
+CorrectPassword = "imran"
 
 loop = 'true'
 while (loop == 'true'):
